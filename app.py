8627ffc09089ed10833aaf81aa28e274e972d48c
Licas59 <cordiez.enzo@gmail.com>
2025-12-23 15:27:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection 
 
 st.set_page_config(page_title="Family", page_icon="🏠")
-st.title("🏠Dashboard de la Famille🏠")
+st.title("La Family🏠")
 
 conn = st.connection("gsheets", type = GSheetsConnection)
 url = "https://docs.google.com/spreadsheets/d/1-GcU9NzoVJd2KMQmnQPjnH2VxJOR7sIMe49Jkg78ynA/edit?usp=drivesdk"
@@ -11,3 +11,5 @@ user = st.sidebar.text_input("Qui est tu ?")
 if not user :
     st.warning("Veuillez entrer votre nom dans le menu à gauche. ")
     st.stop()
+data = conn.read(spreadsheet=url)
+st.dataframe(data)
